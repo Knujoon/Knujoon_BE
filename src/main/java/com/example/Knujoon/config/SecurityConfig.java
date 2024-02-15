f2ebc8e6409056826aab75d014ca73e2ac9d2eac
DongDongShin <tlsehdgk87@naver.com>
2024-02-15 23:37:30 +0900

    feat: h2. permitAll

--- src/main/java/com/example/Knujoon/config/SecurityConfig.java
+++ src/main/java/com/example/Knujoon/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.example.Knujoon.jwt.JwtSecurityConfig;
 import com.example.Knujoon.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -64,8 +65,9 @@ public class SecurityConfig {
                 // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
                 .authorizeHttpRequests()
+
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
                 .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
