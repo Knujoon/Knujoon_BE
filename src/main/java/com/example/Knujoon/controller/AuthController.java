47c0cdefa8a9b5e12a80ca8a8ff6f33fe82afbac
DongDongShin <tlsehdgk87@naver.com>
2024-02-16 01:47:14 +0900

    feat: 이메일 전송기능, 중복 검사기능 추가

--- src/main/java/com/example/Knujoon/controller/AuthController.java
+++ src/main/java/com/example/Knujoon/controller/AuthController.java
@@ -1,26 +1,22 @@
 package com.example.Knujoon.controller;
 
-import com.example.Knujoon.dto.MemberRequestDto;
-import com.example.Knujoon.dto.MemberResponseDto;
-import com.example.Knujoon.dto.TokenDto;
-import com.example.Knujoon.dto.TokenRequestDto;
+import com.example.Knujoon.dto.*;
 import com.example.Knujoon.service.AuthService;
+import com.example.Knujoon.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final EmailService emailService;
 
     @PostMapping("/signup")
-    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) {
-        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    public ResponseDto<?> signup(@RequestBody MemberRequestDto memberRequestDto) {
+        return authService.signup(memberRequestDto);
     }
 
     @PostMapping("/login")
@@ -29,8 +25,23 @@ public class AuthController {
         return ResponseEntity.ok(authService.login(memberRequestDto));
     }
 
+    @PostMapping("email/send")
+    public ResponseDto<?> sendEmail(@RequestParam("email") String email) throws Exception {
+        emailService.sendSimpleMessage(email);
+        return ResponseDto.success("이메일 전송 완료");
+    }
+
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
     }
+
+    @GetMapping("/valid_id")//백준 아이디의 중복을 검사해줌요
+    public ResponseDto<?> confirmId(@RequestBody UserIdRequestDto requestDto){
+        return authService.confirmId(requestDto);
+    }
+
+
+
 }
