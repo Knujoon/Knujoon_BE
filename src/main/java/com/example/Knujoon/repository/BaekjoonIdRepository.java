caa34248d285a4cf0f592f17001d5f75003471b7
DongDongShin <tlsehdgk87@naver.com>
2024-02-16 01:48:39 +0900

    feat: 백준아이디로 search

--- src/main/java/com/example/Knujoon/repository/BaekjoonIdRepository.java
+++ src/main/java/com/example/Knujoon/repository/BaekjoonIdRepository.java
@@ -3,7 +3,11 @@ package com.example.Knujoon.repository;
 import com.example.Knujoon.entity.BaekjoonId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BaekjoonIdRepository extends JpaRepository<BaekjoonId,Long> {
+
+    boolean existsByUserId(String userId);
 
 
 }
