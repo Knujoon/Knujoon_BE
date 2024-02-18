2188192ca7f0c63b87ab0d81cafb0e9bf4f52f8a
DongDongShin <tlsehdgk87@naver.com>
2024-02-19 00:59:09 +0900

    feat: Scheduling 추가

--- src/main/java/com/example/Knujoon/KnujoonApplication.java
+++ src/main/java/com/example/Knujoon/KnujoonApplication.java
@@ -3,8 +3,10 @@ package com.example.Knujoon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling//스케줄링 가능하게
 public class KnujoonApplication {
 
 	public static void main(String[] args) {
